cf28e6395ddc4c8fb4707e7db3f602fc7c16f745
oussamabenali4 <oussama.benali@ump.ac.ma>
2024-02-11 22:57:25 +0100

    this aprouch seems better

--- tests/main.go
+++ tests/main.go
@@ -13,40 +13,46 @@ func main() {
 
 func solution(y int, board [8]int, sl []int) [8]int {
 	for i := 1; i <= 8; i++ {
-		if !in(i, sl) { //&& i != sl[y-1] && i != sl[y-1]-1 {
-			if !diag(i, sl) {
-				sl = append(sl, i)
-				board[y] = i
-				if y < 7 {
-					return solution(y+1, board, sl)
-				} else {
-					return board
-				}
+		//if !in(i, sl) { //&& i != sl[y-1] && i != sl[y-1]-1 {
+		if !diag(i, y, board) {
+			sl = append(sl, i)
+			board[y] = i
+			if y < 7 {
+				return solution(y+1, board, sl)
+			} else {
+				return board
 			}
 		}
+		//}
 	}
 	return board
 }
-func in(i int, sl []int) bool {
-	if len(sl) == 0 {
-		return false
-	}
-	for _, item := range sl {
-		if item == i {
-			return true
-		}
-	}
-	return false
-}
 
-func diag(i int, sl []int) bool { // this needs further devlopment; 
-	if len(sl) == 0 {
+// func in(i int, sl []int) bool {
+// 	if len(sl) == 0 {
+// 		return false
+// 	}
+// 	for _, item := range sl {
+// 		if item == i {
+// 			return false // true
+// 		}
+// 	}
+// 	return false
+// }
+
+// i + y - j
+func diag(i int, y int, board [8]int) bool { // this needs further devlopment;
+	if board[0] == 0 {
 		return false
 	}
-	for j, item := range sl {
-		if item == i+j+1 || item == i-j-1 {
+	for j := 0; j < y; j++ {
+		if board[j] == 0 {
+			return false
+		}
+		if board[j]+y-j == i || board[j]-y+j == i || board[j] == i { // i need to fix this equation
 			return true
 		}
+
 	}
 	return false
 }

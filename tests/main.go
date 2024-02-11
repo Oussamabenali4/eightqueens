7ffc7c005f7ba845ebc75bbc6a28fa8d882c7489
oussamabenali4 <oussama.benali@ump.ac.ma>
2024-02-11 23:30:35 +0100

    finally the first solution :)

--- tests/main.go
+++ tests/main.go
@@ -3,45 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var sl []int
 	var board [8]int
-	board = solution(0, board, sl)
+	board = solution(0, 1, board)
 	fmt.Println(board)
 }
 
-// the enitial value of y will be 0 and will be increasing
-
-func solution(y int, board [8]int, sl []int) [8]int {
-	for i := 1; i <= 8; i++ {
-		//if !in(i, sl) { //&& i != sl[y-1] && i != sl[y-1]-1 {
+func solution(y int, start int, board [8]int) [8]int {
+	for i := start; i <= 8; i++ {
 		if !diag(i, y, board) {
-			sl = append(sl, i)
 			board[y] = i
 			if y < 7 {
-				return solution(y+1, board, sl)
+				return solution(y+1, 1, board)
 			} else {
 				return board
 			}
+		} else {
+			if i < 8 {
+				continue
+			} else {
+				if board[y-1] == 8 {
+					return solution(y-2, board[y-2]+1, board)
+				}
+				return solution(y-1, board[y-1]+1, board)
+			}
 		}
-		//}
 	}
 	return board
 }
 
-// func in(i int, sl []int) bool {
-// 	if len(sl) == 0 {
-// 		return false
-// 	}
-// 	for _, item := range sl {
-// 		if item == i {
-// 			return false // true
-// 		}
-// 	}
-// 	return false
-// }
-
-// i + y - j
-func diag(i int, y int, board [8]int) bool { // this needs further devlopment;
+func diag(i int, y int, board [8]int) bool {
 	if board[0] == 0 {
 		return false
 	}
@@ -49,7 +39,7 @@ func diag(i int, y int, board [8]int) bool { // this needs further devlopment;
 		if board[j] == 0 {
 			return false
 		}
-		if board[j]+y-j == i || board[j]-y+j == i || board[j] == i { // i need to fix this equation
+		if board[j]+y-j == i || board[j]-y+j == i || board[j] == i {
 			return true
 		}
 

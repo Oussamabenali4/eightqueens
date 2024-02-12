3d51117289c62f92e4df370d741c9edfb5cc1c3c
oussamabenali4 <oussama.benali@ump.ac.ma>
2024-02-12 20:36:42 +0100

    this is the solution and i need to make it better and faster if possible

--- main.go
+++ main.go
@@ -1,40 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var board [8]int
-	board = solution(0, 1, board)
-	fmt.Println(board)
+	var solutions [][8]int
+	y := 0
+	y, board, solutions = solution(y, 1, board, solutions)
+	fmt.Println(solutions)
+	fmt.Println(len(solutions))
 }
 
-func solution(y int, start int, board [8]int) [8]int {
-	for i := start; i <= 8; i++ {
-		if !diag(i, y, board) {
+func solution(y int, start int, board [8]int, solutions [][8]int) (int, [8]int, [][8]int) { // i should pass the function as paramiter .
+	for i := start; i <= 8; i++ { // this is for the possible solutions
+		if !diag(i, y, board) { // yo check if the solution is valid
 			board[y] = i
 			if y < 7 {
-				return solution(y+1, 1, board)
+				return solution(y+1, 1, board, solutions) // stil has work to do
 			} else {
-				return board
+				solutions = append(solutions, board)
+				if board[y-1] == 8 { // return to the previous
+					if y-1 == 0 {
+						return y, [8]int{}, solutions
+					}
+					return solution(y-2, board[y-2]+1, board, solutions)
+				}
+				return solution(y-1, board[y-1]+1, board, solutions)
+				//return y, board, solutions // done with the solution // here i need to find a way to move to the next
 			}
 		} else {
-			if i < 8 {
+			if i < 8 { // if didn't find any solution just move to the next possible
 				continue
-			} else {
-				if board[0] == 8 {
-					return [8]int{}
+			} else { // but if there is no solution at all
+				if board[0] == 8 && board[1] == 6 && board[2] == 4 { // you just stop if there will be no solutions and return the signal
+					return 0, [8]int{}, solutions
 				}
-				if board[y-1] == 8 {
+				if board[y-1] == 8 { // return to the previous
 					if y-1 == 0 {
-						return [8]int{}
+						return y, [8]int{}, solutions
 					}
-					return solution(y-2, board[y-2]+1, board)
+					return solution(y-2, board[y-2]+1, board, solutions)
 				}
-				return solution(y-1, board[y-1]+1, board)
+				return solution(y-1, board[y-1]+1, board, solutions)
 			}
 		}
 	}
-	return board
+	return y, board, solutions
 }
 
 func diag(i int, y int, board [8]int) bool {
